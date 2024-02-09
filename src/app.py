df7320398c89681130e2ec6ce655b4ad3cc33916
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2024-02-09 22:54:24 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -25,7 +25,7 @@ class data:
              }
     def Genre(self,genre,lang):
         with engine.connect() as connection:
-            query= text(f"SELECT id,title,poster_path,recommendations FROM main_table WHERE original_language='{lang}' AND genres LIKE '%{genre}%' AND vote_average>=7 AND poster_path IS NOT NULL ORDER BY release_date DESC,vote_average DESC, popularity DESC;")
+            query= text(f"SELECT id,title,poster_path,recommendations FROM main_table WHERE original_language='{lang}' AND genres LIKE '%{genre}%' AND vote_average>=7 AND poster_path IS NOT NULL AND recommendations IS NOT NULL ORDER BY release_date DESC,vote_average DESC, popularity DESC;")
             result = connection.execute(query)
             
             for row in result:
@@ -47,7 +47,7 @@ class data:
         return content
     def Release(self,Year,lang):
         with engine.connect() as connection:
-            query= text(f"SELECT id,title,poster_path,recommendations FROM main_table WHERE original_language='{lang}' AND release_date BETWEEN DATE '{Year.year}-01-01' AND DATE '{Year}'  AND poster_path IS NOT NULL ORDER BY release_date DESC,vote_average DESC,result ASC;")
+            query= text(f"SELECT id,title,poster_path,recommendations FROM main_table WHERE original_language='{lang}' AND release_date BETWEEN DATE '{Year.year}-01-01' AND DATE '{Year}'  AND poster_path IS NOT NULL AND recommendations IS NOT NULL ORDER BY release_date DESC,vote_average DESC,result ASC;")
             result = connection.execute(query)
             
             for row in result:
@@ -60,7 +60,7 @@ class data:
     def Randomize(self,value,lang):
         
         with engine.connect() as connection:
-            query = text(f"SELECT id,title,poster_path,recommendations FROM main_table WHERE vote_average>={value} AND original_language='{lang}' AND release_date>= DATE '2018-01-01'  AND poster_path IS NOT NULL ORDER BY result ASC, vote_average DESC;")
+            query = text(f"SELECT id,title,poster_path,recommendations FROM main_table WHERE vote_average>={value} AND original_language='{lang}' AND release_date>= DATE '2018-01-01'  AND poster_path IS NOT NULL AND recommendations IS NOT NULL ORDER BY result ASC, vote_average DESC;")
             result = connection.execute(query)
             
             for row in result:
